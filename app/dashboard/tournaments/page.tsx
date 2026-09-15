69a26d610a8e972933747395172724dc0b8470f1
Farhan Ahmed Khan/IT/HO <farhan.ahmed@tcs.com.pk>
2026-09-15 12:07:59 +0500

    ui update

--- app/dashboard/tournaments/page.tsx
+++ app/dashboard/tournaments/page.tsx
@@ -1308,7 +1308,7 @@ export default function TournamentsPage() {
                     </>
                   )}
                 </div>
-                <div className="grid grid-cols-3 gap-4">
+                <div className="grid sm:grid-cols-3 grid-cols-1  gap-4">
                   {/* Entry Fee */}
                   <div>
                     <Label
